37f1a14085a5fd3a2e54f62f08162071d5447044
sincekmori <sincekmori@gmail.com>
2023-05-21 19:04:44 +0900

    remove tests

--- tests/azure/test_stable_2023_05_15.py
+++ tests/azure/test_stable_2023_05_15.py
@@ -2,47 +2,13 @@ import os
 
 import pytest
 
-from gpru.azure.stable_2023_05_15 import (
-    AzureOpenAiApi,
-    ChatCompletionRequest,
-    CompletionRequest,
-    UserMessage,
-)
+from gpru.azure.stable_2023_05_15 import AzureOpenAiApi
 from gpru.exceptions import ApiError
 
 PREFIX = "AZURE_OPENAI_API"
 endpoint = os.environ[f"{PREFIX}_ENDPOINT"]
 api_key = os.environ[f"{PREFIX}_KEY"]
 api = AzureOpenAiApi(endpoint, api_key, timeout=30)
-
-completion_deployment_id = os.environ[f"{PREFIX}_COMPLETION_DEPLOYMENT_ID"]
-chat_completion_deployment_id = os.environ[f"{PREFIX}_CHAT_COMPLETION_DEPLOYMENT_ID"]
-
-
-def test_completion() -> None:
-    req = CompletionRequest(prompt="Say this is a test.")
-    completion = api.create_completion(completion_deployment_id, req)
-    assert len(completion.text) > 0  # type: ignore[union-attr]
-
-
-def test_completion_stream() -> None:
-    req = CompletionRequest(prompt="Say this is a test.", stream=True)
-    for completion in api.create_completion(completion_deployment_id, req):
-        assert type(completion.text) == str  # type: ignore[union-attr]
-
-
-def test_chat_completion() -> None:
-    req = ChatCompletionRequest(messages=[UserMessage("Hello!")])
-    chat_completion = api.create_chat_completion(chat_completion_deployment_id, req)
-    assert len(chat_completion.content) > 0  # type: ignore[union-attr]
-
-
-def test_chat_completion_stream() -> None:
-    req = ChatCompletionRequest(messages=[UserMessage("Hello!")], stream=True)
-    for chat_completion in api.create_chat_completion(
-        chat_completion_deployment_id, req
-    ):
-        assert type(chat_completion.content) == str  # type: ignore[union-attr]
 
 
 def test_models() -> None:
